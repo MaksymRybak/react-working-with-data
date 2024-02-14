5342867c52c255d8771c0ddda890f032b052832c
Maksym Rybak <maksym.rybak@ynap.com>
2024-02-14 16:37:57 +0100

    add speaker crud

--- src/lib/general-types.ts
+++ src/lib/general-types.ts
@@ -6,4 +6,49 @@ export interface Speaker {
   twitterHandle: string;
   userBioShort: string;
   timeSpeaking: Date;
+  favoriteCount?: number;
+  favorite?: boolean;
+  favoriteStatus?: FavoriteStatusEnum;
+  sessions?: SpeakerSession[];
+  favorites?: AttendeeFavorite[];
+  _count?: { favorites: number };
 }
+
+export interface Session {
+  id: number;
+  title: string;
+  description: string;
+  sessionStart?: Date;
+  speakers?: SpeakerSession[];
+}
+
+export interface SpeakerSession {
+  speakerId: number;
+  sessionId: number;
+  speaker: Speaker;
+  session: Session;
+}
+
+export interface Attendee {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  createdDate: Date;
+  favorites?: AttendeeFavorite[];
+}
+
+export interface AttendeeFavorite {
+  id: number;
+  attendeeId: string;
+  speakerId: number;
+  attendee: Attendee;
+  speaker: Speaker;
+}
+
+type FavoriteStatusEnum =
+  | "favored"
+  | "not-favored"
+  | "updating"
+  | "hidden"
+  | "errored";
